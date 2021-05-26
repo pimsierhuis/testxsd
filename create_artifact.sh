4c0a05becf6d077d18f016added83c21ecfcb8a0
Pim Sierhuis <pim@pimsierhuis.nl>
2021-05-26 21:22:37 +0200

    bla

---
+++ create_artifact.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+rm -rf target
+mkdir -p target/build
+
+
+### Determine version from git info
+
+echo "Determining version..."
+
+VERSION=0-localbuild
+
+if [ -z "$GITHUB_REF" ]; then
+  echo "Github ref not set, using version 0-localbuild"
+  
+else
+  echo "Github ref: $GITHUB_REF"
+  if [[ $GITHUB_REF == refs/heads/* ]]; then
+    BRANCH=${GITHUB_REF:11}
+    VERSION=branch-${BRANCH}-${GITHUB_SHA}
+  elif [[ $GITHUB_REF == refs/tags/* ]]; then
+    VERSION=${GITHUB_REF:10}
+  else
+    echo "Unsupported github ref, exiting"
+    exit 1
+  fi
+fi
+
+echo "Using version $VERSION"
+
+
+### Build
+
+sed s/PLACEHOLDER/${VERSION}/ test.xsd > target/build/test.xsd
+
+### Create artifact
+
+ARTIFACT_FILE=testxsd-${VERSION}.zip
+
+mkdir -p target/artifact
+(cd target/build && zip -r ../../target/artifact/${ARTIFACT_FILE} .)
+
